eace1aa70b95136dca8f87b5e3d2babed5bed5fc
jtorova5 <jtorova5@gmail.com>
2024-07-18 08:48:49 -0500

    feat: se carga diagrama de clases y se crea la estructura inicial de carpetas y archivos

---
+++ Sistema_de_gestion_de_biblioteca/Models/Libro.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema_de_gestion_de_biblioteca.Models;
+
+public class Libro
+{
+
+}
